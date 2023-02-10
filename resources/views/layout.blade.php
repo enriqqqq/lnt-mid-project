9984ec33bf6f0afd4ae41c14839042af2e3644f4
Enrique Heryanto <108193211+enriqqqq@users.noreply.github.com>
2023-02-10 14:15:38 +0700

    Added layout

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    @yield('head')
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
+</head>
+<body>
+    <div class="container">
+        <header>
+            <div class="logo">
+                <img class="logo" src="{{asset('images/logo-white.png')}}" alt="">
+                <p class="title">Library</p>
+            </div>
+            <div class="btn">
+                @yield('button')
+            </div>
+        </header>
+
+        <main>
+            @yield('main')
+        </main>
+
+        <footer>
+            <p>&#169 2023: Footer.com</p>
+        </footer>
+    </div>
+</body>
+</html>
