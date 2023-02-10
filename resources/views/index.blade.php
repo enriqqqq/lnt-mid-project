7ea22e7569e9ec4386cedc307a0e5aa0ac2607b1
Enrique Heryanto <108193211+enriqqqq@users.noreply.github.com>
2023-02-10 18:05:28 +0700

    Created data passing to index

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -8,29 +8,27 @@
     <button id= "reroute" type="button" href="#"> Add Page </button>
 @endsection
 @section('main')
-    <div class="display">
-
-        <div class="card">
-            <div class="btn-row" type="button">
-                <button class="update">
-                    <img src="{{asset('images/edit.png')}}" alt="" class="btn">
-                </button>
-                <button class="delete" type="button">
-                    <img src="{{asset('images/delete.png')}}" alt="" class="btn">
-                </button>
-            </div>
-            <img class="profile" src="{{asset('images/no-profile.png')}}" alt= "">
-            <p>EnriqueHeryanto12345</p>
-        </div>
-
-        <div class="card">test</div>
-        <div class="card">test</div>
-        <div class="card">test</div>
-        <div class="card">test</div>
-        <div class="card">test</div>
-        <div class="card">test</div>
-        <div class="card">test</div>
-        <div class="card">test</div>
-        <div class="card">test</div>
+    @if(count($employees) == 0)
+    <div class="noentry-wrapper">
+        <p>No Entries Found</p>            
     </div>
+    @else
+        <div class="display">
+            @foreach($employees as $employee)
+                <div class="card">
+                    <div class="btn-row" type="button">
+                        <button class="update">
+                            <img src="{{asset('images/edit.png')}}" alt="" class="btn">
+                        </button>
+                        <button class="delete" type="button">
+                            <img src="{{asset('images/delete.png')}}" alt="" class="btn">
+                        </button>
+                    </div>
+                    <img class="profile" src="{{asset('images/no-profile.png')}}" alt= "">
+                    <p>{{$employee->name}}</p>
+                </div>
+            @endforeach
+        </div>
+    @endif
+
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Models\Employee;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -14,5 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('index');
+    return view('index', [
+        'employees' => Employee::all()
+    ]);
 });
